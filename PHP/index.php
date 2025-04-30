15a61b71cc0a640aadc890ddb51f2cc51375b40e
Sayem2004 <mdalimransayem@gmail.com>
2025-04-30 09:57:46 +0600

    Practices PHP

---
+++ PHP/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hellow php</h1>
+    <h1><?php echo "Hellow"; ?> php </h1>
+
+    <?php 
+    echo "Rakib,  Sakib<br>";  
+    echo "Raju<br>";
+    echo "Sayem<br><br>";
+
+    
+  
+    ?>
+
+<!-- Variable -->
+ <?php 
+ $name = "Name= Zim <br>";
+ $price = "Price= 100<br>";
+ $address = "Address= c-block bashundhara<br>";
+ $text="<br>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis voluptatem mollitia adipisci,
+ perferendis dignissimos explicabo rerum nesciunt temporibus assumenda voluptates deserunt ut nulla odit 
+ porro suscipit cupiditate. Vitae, eligendi tenetur?";
+ 
+ echo $name;
+ echo $price;
+ echo $address;
+ echo $text;
+ ?>
+   
+
+    
+</body>
+</html>
