b5d720518a25b5200d3bc5a3194e0dd4076ab28a
Sayem2004 <mdalimransayem@gmail.com>
2025-05-09 11:19:02 +0600

    Practices

---
+++ JavaScript/number.js
@@ -0,0 +1,15 @@
+var num =20;
+
+num = toString(num);
+console.log(typeof(num));
+
+//Example
+
+var number = 2.012123;
+console.log(number.toFixed(4));
+console.log(number.toPrecision(4));
+
+console.log(typeof(Number("4553")));
+console.log(Number("4553"));
+console.log(Number(true));
+console.log(Number(false));
---
+++ JavaScript/variables.js
@@ -0,0 +1,17 @@
+
+console.log("Personal Info");
+console.log("MD. AL-IMRAN SAYEM");
+console.log(22004.25) //earn
+console.log(52000.00)//expense
+
+//
+
+const myName ="Sayem";
+const income = 12345.23;
+const expense =15766.00;
+
+console.log("Personal Info2");
+alert(myName);
+console.log(income);
+console.log(expense);
+
